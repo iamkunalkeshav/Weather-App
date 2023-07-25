842885782fe9f7b01cf359afd9102bf8ce929569
Kunal Keshav <77605040+iamkunalkeshav@users.noreply.github.com>
2023-07-25 20:27:05 +0530

    code updated

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "{c5970051aaf84f2684b114424232507}",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
